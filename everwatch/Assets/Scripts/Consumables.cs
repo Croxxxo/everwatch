fbb0c1db152842e6d1c1d05632a6c3634fc599ae
Blake <ljames0120make@gmail.com>
2019-05-15 10:22:05 -0400

    Yay unity

---
+++ everwatch/Assets/Scripts/Consumables.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Consumables : MonoBehaviour
+{
+    public enum ConsumableType {Food, Water};
+    public ConsumableType consumeableType;
+    public int restoreAmount;
+    public PlayerVitals pv;
+
+    public bool eat;
+
+    public void Start()
+    {
+        pv = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerVitals>();
+    }
+
+    public void Update()
+    {
+        if (eat)
+            EatItem();
+    }
+
+    public void EatItem()
+    {
+        if (consumeableType == ConsumableType.Food)
+        {
+            pv.hunger -= restoreAmount;
+            Destroy(this);
+        } else if(consumeableType == ConsumableType.Water)
+        {
+            pv.thirst -= restoreAmount;
+            Destroy(this);
+        }
+    }
+}
