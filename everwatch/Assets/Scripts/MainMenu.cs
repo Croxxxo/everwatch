4f8001f05e17233022a535ffbc1e84831eaf9820
Colton <CPerham@NF-LAB-413L.local.fehb.org>
2019-06-10 09:57:59 -0400

    made dead scene

--- everwatch/Assets/Scripts/MainMenu.cs
+++ everwatch/Assets/Scripts/MainMenu.cs
@@ -25,4 +25,9 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("QUIT!");
     }
+
+    public void Restart ()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
 }
